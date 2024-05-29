0021ecadf8deef960416339895d2ea9180e3f4f3
swchangdev <swchangdev@gmail.com>
2024-05-29 13:18:26 +0900

    QQ-90 first implementation of connection pool invalidation

--- src/protocol/connection_pool.cc
+++ src/protocol/connection_pool.cc
@@ -1,21 +1,34 @@
 #include "connection_pool.h"
 
+#include <cassert>
 #include "connection_desc.h"
 
 namespace quokkaquery {
 namespace protocol {
-static inline const bool CheckOccupied(ConnectionPool::ConnectionHandlePtr ptr) {
+using ConnectionHandlePtr = ConnectionPool::ConnectionHandlePtr;
+using ConnectionMap = ConnectionPool::Map;
+using ConnectionList = ConnectionPool::List;
+
+static inline const bool CheckOccupied(ConnectionHandlePtr& ptr) {
   /* Unoccupied only if it has null worker handle */
   return !ptr && !(ptr->worker_handle);
 }
 
-static inline ConnectionPool::ConnectionHandlePtr GetInvalidConnHandlePtr() {
-  return ConnectionPool::ConnectionHandlePtr(nullptr);
+static inline ConnectionHandlePtr InvalidConnHandlePtr() {
+  return ConnectionHandlePtr(nullptr);
+}
+
+static inline std::vector<ConnectionList::iterator>& Array(ConnectionMap::iterator iter) {
+  return iter->second.first;
+}
+
+static inline std::size_t& RealArraySize(ConnectionPool::Map::iterator iter) {
+  return iter->second.second;
 }
 
 ConnectionPool::ConnectionPool(std::size_t pool_size)
     : map_(pool_size),
-      lru_list_(pool_size),
+      lru_list_(),
       size_(0),
       capability_(pool_size) {}
 
@@ -26,7 +39,7 @@ ConnectionPool::~ConnectionPool() {
 }
 
 const bool ConnectionPool::Empty() const {
-  return size_ == 0;
+  return map_.empty();
 }
 
 const std::size_t ConnectionPool::Size() const {
@@ -39,89 +52,112 @@ const std::size_t ConnectionPool::Capability() const {
 
 void ConnectionPool::Resize(std::size_t new_size) {
   map_.rehash(new_size);
-  lru_list_.resize(new_size);
   capability_ = new_size;
 }
 
-ConnectionPool::ConnectionHandlePtr ConnectionPool::Emplace(const ConnectionDesc& desc) {
+ConnectionHandlePtr ConnectionPool::Emplace(const ConnectionDesc& desc) {
   if (size_ == capability_) {
     InvalidateLRU();
   }
 
-  auto iter = map_.find(desc);
-  std::size_t npos = iter == map_.end() ? 0 : iter->second.size();
-
-  return EmplaceInternal(desc, npos);
+  return EmplaceInternal(desc);
 }
 
-ConnectionPool::ConnectionHandlePtr ConnectionPool::Reuse(const ConnectionDesc& desc) {
-  auto iter = map_.find(desc);
-  if (iter == map_.end()) {
-    return GetInvalidConnHandlePtr();
+ConnectionHandlePtr ConnectionPool::Reuse(const ConnectionDesc& desc) {
+  if (!map_.contains(desc)) {
+    return InvalidConnHandlePtr();
   }
 
-  auto conn_ptr = GetInvalidConnHandlePtr();
-  for (auto& conn : iter->second) {
-    if (!CheckOccupied(conn)) {
-      conn_ptr = conn;
+  return ReuseInternal(desc);
+
+}
+
+ConnectionHandlePtr ConnectionPool::ReuseOrEmplace(const ConnectionDesc& desc) {
+  if (!map_.contains(desc)) {
+    return EmplaceInternal(desc);
+  }
+
+  return Reuse(desc);
+}
+
+void ConnectionPool::Invalidate() {
+  lru_list_.clear();
+  map_.clear();
+}
+
+ConnectionHandlePtr ConnectionPool::EmplaceInternal(const ConnectionDesc& desc) {
+  if (!map_.contains(desc)) {
+    map_.emplace(std::make_pair(desc, std::make_pair(std::vector<List::iterator>{}, 0)));
+  }
+
+  auto iter = map_.find(desc);
+  auto& array = Array(iter);
+  std::size_t npos = 0;
+  for (; npos < array.size(); ++npos) {
+    if (array[npos] == lru_list_.end()) {
+      lru_list_.emplace_front(std::make_shared<ConnectionHandle>(desc, npos));
+      array[npos] = lru_list_.begin();
+    }
+  }
+
+  lru_list_.emplace_front(std::make_shared<ConnectionHandle>(desc, npos));
+  array.emplace_back(lru_list_.begin());
+
+  ++RealArraySize(iter);
+  ++size_;
+
+  return lru_list_.front();
+}
+
+ConnectionHandlePtr ConnectionPool::ReuseInternal(const ConnectionDesc& desc) {
+  auto iter = map_.find(desc);
+  auto conn_ptr = InvalidConnHandlePtr();
+
+  for (auto& lru_list_iter : Array(iter)) {
+    auto& ptr = *lru_list_iter;
+    if (!CheckOccupied(ptr)) {
+      lru_list_.erase(lru_list_iter);
+      lru_list_.push_front(ptr);
+      lru_list_iter = lru_list_.begin();
+      
+      conn_ptr = ptr;
     }
   }
 
   return conn_ptr;
 }
 
-ConnectionPool::ConnectionHandlePtr ConnectionPool::ReuseOrEmplace(const ConnectionDesc& desc) {
-  auto iter = map_.find(desc);
-
-  if (iter == map_.end()) {
-    return EmplaceInternal(desc, 0);
-  }
-
-  return Reuse(desc);
-}
-
-ConnectionPool::ConnectionHandlePtr ConnectionPool::Get(ConnectionHandle::Locator loc) {
-  auto iter = map_.find(loc.conn_desc);
-
-  if (iter == map_.end()) {
-    return GetInvalidConnHandlePtr();
-  }
-
-  auto &array = iter->second;
-  if (array.size() <= loc.npos) {
-    return GetInvalidConnHandlePtr();
-  }
-
-  return array[loc.npos];
-}
-
-void ConnectionPool::Invalidate(const ConnectionHandle::Locator&) {
-  /* TODO QQ-90 */
-  return;
-}
-
-void ConnectionPool::Invalidate(const ConnectionDesc&) {
-  /* TODO QQ-90 */
-  return;
-}
-
-void ConnectionPool::Invalidate() {
-  /* TODO QQ-90 */
-  return;
-}
-
 void ConnectionPool::InvalidateLRU() {
-  /* TODO QQ-90 */
+  auto iter = lru_list_.rbegin();
+  for (; iter != lru_list_.rend(); ++iter) {
+    auto& ptr=  *iter;
+    if (!CheckOccupied(ptr)) {
+      return InvalidateInternal(ptr->locator);
+    }
+  }
+
+  /* Too many occupied connections */
   throw std::exception();
 }
 
-ConnectionPool::ConnectionHandlePtr ConnectionPool::EmplaceInternal(const ConnectionDesc& desc,
-                                                                    const std::size_t npos) {
-  auto& new_entry = map_[desc].emplace_back(std::make_shared<ConnectionHandle>(desc, npos));
-  lru_list_.emplace_back(new_entry);
-  ++size_;
 
-  return new_entry;
+void ConnectionPool::InvalidateInternal(ConnectionHandle::Locator locator) {
+  auto iter = map_.find(locator.conn_desc);
+  auto& array = Array(iter);
+  auto& lru_list_iter = array[locator.npos];
+
+  if (lru_list_iter == lru_list_.end()) {
+    return;
+  }
+
+  lru_list_.erase(lru_list_iter);
+  lru_list_iter = lru_list_.end();
+  --RealArraySize(iter);
+  --size_;
+
+  if (RealArraySize(iter) == 0) {
+    array.clear();
+  }
 }
 }  // namespace protocol
 }  // namespace quokkaquery
